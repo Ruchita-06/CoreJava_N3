6b776ddf3c746ebae093b99f7ed18b6febf29cbf
RUCHITA <RUCHITA@DESKTOP-BMS8ESG>
2023-02-02 19:33:29 +0530

    Second Commit

--- DayOne/src/org/tnsif/intro/UserInput.java
+++ DayOne/src/org/tnsif/intro/UserInput.java
@@ -2,7 +2,7 @@ package org.tnsif.intro;
 
 import java.util.Scanner;
 
-public class UserInput {
+public class UserInput{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -12,15 +12,15 @@ public class UserInput {
 		char z=sc.next().charAt(0);
 		String name=sc.next();
 		String str=sc.nextLine();
-		double d=sc.nextDouble();
+		//double d=sc.nextDouble();
 		
 		System.out.println("The value of x is:"+x);
 		System.out.println("The value of y is:"+y);
 		System.out.println("The value of z is:"+z);
 		System.out.println("The value of name is:"+name);
 		System.out.println("The value of str is:"+str);
-		System.out.println("The value of d is:"+d);
-		sc.close();
+		//System.out.println("The value of d is:"+d);
+		
 	}
 
 }
