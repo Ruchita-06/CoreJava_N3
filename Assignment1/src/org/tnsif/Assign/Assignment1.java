85cbdaf01a77267d0e390681453f24a539e1a6f6
RUCHITA <RUCHITA@DESKTOP-BMS8ESG>
2023-02-04 19:00:27 +0530

    Fifth Commit

---
+++ Assignment1/src/org/tnsif/Assign/Assignment1.java
@@ -0,0 +1,15 @@
+package org.tnsif.Assign;
+import java.util.Scanner;
+public class Assignment1 {
+	public static void main(String[] args) {
+		      Scanner sc = new Scanner(System.in);
+		      int count = 0;
+		      System.out.println("Enter a number:");
+		      int num = sc.nextInt();
+		      while(num!=0){
+		         num = num/10;
+		         count++;
+		      }
+		      System.out.println("Number of digits in the entered integer are:"+count);
+		   }
+		}
